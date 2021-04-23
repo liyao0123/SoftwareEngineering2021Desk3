19edfb3c096c60a6e09711d6d3993f9ff3e74642
Darren668 <xinhaojiegryx@163.com>
2021-04-23 20:57:45 +0100

    basic configuration in web project

---
+++ efww/src/main/java/team/desk3/efww/EndangeredFaunaWorldWindowApplication.java
@@ -0,0 +1,13 @@
+package team.desk3.efww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndangeredFaunaWorldWindowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndangeredFaunaWorldWindowApplication.class, args);
+    }
+
+}
---
+++ efww/src/test/java/team/desk3/efww/EndangeredFaunaWorldWindowApplicationTests.java
@@ -0,0 +1,13 @@
+package team.desk3.efww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndangeredFaunaWorldWindowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
