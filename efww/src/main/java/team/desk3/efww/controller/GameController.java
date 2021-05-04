50f842e410b1c71f94f1662920bac65da2cad4c9
Darren668 <xinhaojiegryx@163.com>
2021-05-04 04:04:32 +0100

    improve some layout and add some new interactions with players

--- efww/src/main/java/team/desk3/efww/controller/GameController.java
+++ efww/src/main/java/team/desk3/efww/controller/GameController.java
@@ -21,23 +21,29 @@ public class GameController {
     @Autowired
     FaunaService faunaService;
 
-    @RequestMapping("/game1")
+    @RequestMapping("/game1/1")
     public String toGame1(Model model){
         //get data from the database where kind is null
         QueryWrapper<Fauna> query = Wrappers.query();
         query.isNull("kind");
         List<Fauna> faunaList = faunaService.list(query);
         model.addAttribute("faunaList",faunaList);
+        //add the query number to the model
+        model.addAttribute("cardNum",faunaList.size());
         return "game/game1";
     }
 
-    @RequestMapping("/game2")
+
+
+    @RequestMapping("/game2/1")
     public String toGame2(Model model){
         //first show the kind of Hominidae, monkey
         QueryWrapper<Fauna> query = Wrappers.query();
         query.eq("kind","Hominidae");
         List<Fauna> list = faunaService.list(query);
         model.addAttribute("hominidae",list);
+        //add the query number to the model
+        model.addAttribute("cardNum",list.size());
         return "game/game2";
     }
 }
