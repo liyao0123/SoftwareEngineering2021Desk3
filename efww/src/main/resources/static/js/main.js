767bf3d65862d19e0be677b506933d7784e4a831
Darren668 <xinhaojiegryx@163.com>
2021-05-05 01:56:04 +0100

    decorate the carousel part and contact me part, delete some unuseful js file

--- efww/src/main/resources/static/js/main.js
+++ efww/src/main/resources/static/js/main.js
@@ -39,21 +39,21 @@ $(function() {
     
     //===== Section Menu Active
 
-    // var scrollLink = $('.page-scroll');
-    //     // Active link switching
-    //     $(window).scroll(function() {
-    //     var scrollbarLocation = $(this).scrollTop();
-    //
-    //     scrollLink.each(function() {
-    //
-    //       var sectionOffset = $(this.hash).offset().top - 73;
-    //
-    //       if ( sectionOffset <= scrollbarLocation ) {
-    //         $(this).parent().addClass('active');
-    //         $(this).parent().siblings().removeClass('active');
-    //       }
-    //     });
-    // });
+    var scrollLink = $('.page-scroll');
+        // Active link switching
+        $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+          var sectionOffset = $(this.hash).offset().top - 73;
+
+          if ( sectionOffset <= scrollbarLocation ) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+          }
+        });
+    });
     
     
     //===== Sidebar
