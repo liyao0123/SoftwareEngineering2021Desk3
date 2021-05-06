3a0c5e452927b0a5a2ab2be0b6d574734b31ecdd
liyao <->
2021-05-06 05:02:08 +0100

    add earth

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-mvn -f efww/pom.xml clean
-mvn -f efww/pom.xml package
-docker build -t efww .
-docker run -it --name fauna -d -p 80:8080 efww
+ mvn -f ./efww/pom.xml clean
+ mvn -f ./efww/pom.xml package
+(docker build -t efww -f efwwDockerfile .)
+(docker run -it --name fauna -d -p 80:8080 efww)
+
+(docker build -t earth -f earthDockerfile .)
+(docker run -it --name earth -d -p 3000:80 earth)
--- static/docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -t efww .
-docker run -it --name fauna -d -p 80:8080 efww 
